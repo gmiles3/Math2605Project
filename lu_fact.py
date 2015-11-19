35f6b394c3bffbd05db7ef428c0be4f57e338a7f
gmiles3 <graham.miles@gatech.edu>
2015-11-19 00:14:32 -0500

    init

---
+++ lu_fact.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def lu_fact(matrix):
+	n = len(matrix)
+	if n == 0:
+		return
+	i_matrix = np.identity(n)
+	for col in range(0, n):
+		i = 0
+		for i in range(col, n):
+			if matrix[i][col] != 0:
+				break
+		#matrix[i], matrix[col] = matrix[col], matrix[i]
+		#i_matrix[i], i_matrix[col] = i_matrix[col], i_matrix[i]
+		#i = 0
+		if matrix[i][col] != 0:
+			for k in range(col+1, n):
+				if matrix[k][col] != 0:
+					multiple = matrix[k][col]/matrix[i][col]
+					i_matrix[k][col] = multiple
+					for c in range(0, n):
+						matrix[k][c] = matrix[k][c] - (matrix[i][c]*multiple)
+	return (i_matrix, matrix)
