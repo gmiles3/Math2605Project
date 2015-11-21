42619acd9e4e39b3a2440e5632045d1d93cb8dd0
gmiles3 <graham.miles@gatech.edu>
2015-11-21 16:38:21 -0500

    also generates b matrix

--- pascal_generate.py
+++ pascal_generate.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 def pascal_generate(n):
+	if n < 1:
+		return
 	matrix = np.zeros((n,n))
 	for i in range(1, n+1):
 		for j in range(1, n+1):
 			matrix[i-1][j-1] = factorial((i-1)+(j-1))/(factorial(i-1)*factorial(j-1))
-	return matrix
+	b = np.zeros((n,1))
+	for i in range(0, n):
+		b[i][0] = 1/(i+1)
+	return (matrix, b)
 
 def factorial(num):
 	toReturn = 1;
@@ -13,4 +18,5 @@ def factorial(num):
 		toReturn = toReturn * num
 		num = num - 1
 	return toReturn
+
 
