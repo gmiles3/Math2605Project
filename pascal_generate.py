37050be92cd89d959f170656d11f9449decdefe6
Graham Miles <graham.miles@greatparentsacademy.com>
2015-11-24 20:32:10 -0500

    now usable

--- pascal_generate.py
+++ pascal_generate.py
@@ -2,15 +2,13 @@ import numpy as np
 
 
 def pascal_generate(n):
-    if n < 1:                       # if input size is not greater than 1, return
-        return
-    matrix = np.zeros((n, n))       # start with square matrix of 0's
+    matrix = np.zeros((n, n), float)       # start with square matrix of 0's
     for i in range(1, n + 1):       # for each row
         for j in range(1, n + 1):   # for each col
-            matrix[i - 1][j - 1] = factorial((i - 1) + (j - 1)) / (factorial(i - 1) * factorial(j - 1))     # use provided formula for calculating entry value
-    b = np.zeros((n, 1))            # start with b matrix of 0's
+            matrix[i - 1,j - 1] = factorial((i - 1) + (j - 1)) / (factorial(i - 1) * factorial(j - 1))     # use provided formula for calculating entry value
+    b = np.zeros((n, 1), float)            # start with b matrix of 0's
     for i in range(0, n):           # for each row in b
-        b[i][0] = 1 / (i + 1)       # calculate b entry
+        b[i,0] = 1. / (i + 1)       # calculate b entry
     return (matrix, b)              # return P and b
 
 
